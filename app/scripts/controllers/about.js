7117fd8fb56ffcae385178616e07efe113504830
Matt Elia <Q@Matts-Mac-mini.local>
2017-03-16 19:44:02 -0400

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bakerNetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bakerNetApp
+ */
+angular.module('bakerNetApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bakerNetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bakerNetApp
+ */
+angular.module('bakerNetApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
