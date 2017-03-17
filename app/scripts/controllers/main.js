87eb7632cf71766dd01e08dfbe6c35349f86a9b8
Matt Elia <Q@Matts-Mac-mini.local>
2017-03-17 10:25:00 -0400

    rm unwanted news sources, add thumbnail imgs for each article

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,28 +4,46 @@ var app = angular.module('bakerNetApp');
 
 app.controller('MainCtrl', function ($scope, News) {	
 	
-	var postArray=[];
+	var id;
+	var articleArray = [];
+	var srcArray = [];
+	var srcIgnore = ["buzzfeed","cnn","cnbc","entertainment-weekly","the-huffington-post"];
 
 	News.getSources().then(function(response){
+		
 		var sources = response.data.sources;
+
+		// populate array with source identifiers
 		for (var i = 0; i < sources.length; i++) {
-			var id = sources[i].id;
-			News.getNews(id).then(function success(response) {
+			id = sources[i].id; 
+			srcArray.push(id);
+		}
+
+		// remove unwanted news sources
+		for (var j = 0; j < srcIgnore.length; j++) {
+			if($.inArray(srcIgnore[j],srcArray)){
+				var index = srcArray.indexOf(srcIgnore[j]);
+				srcArray.splice(index, 1);
+			}
+		}
+
+		// get all articles from desired sources
+		for (var i = 0; i < srcArray.length; i++) {
+			id = srcArray[i];
+			News.getArticles(id).then(function (response) {
 				var articles = response.data.articles;
-				for (var i = 0; i < articles.length; i++) {
-					postArray.push(articles[i]);
+				for (var ii = 0; ii < articles.length; ii++) {
+					articleArray.push(articles[ii]);
 				}
-			}).then(function error(err) {
-				console.log(err);
 			});
 		}
 
 	});
-
-	// console.log(postArray);
-	$scope.posts = postArray;
 	
-	setInterval(function(){ $('#TickerTape').animate({left: "-="+$('.panel').parent().parent().width()+""}, 2000); }, 10000);
+	console.log(articleArray);
+	$scope.articles = articleArray;
+		
+	setInterval(function(){ $('#NewsTicker').animate({left: "-="+$('.article-list-item').width() }, 2000); }, 8000);
 
 });
 
--- app/scripts/services/news.js
+++ app/scripts/services/news.js
@@ -8,13 +8,13 @@ app.factory('News', function($http) {
 		return $http.get('https://newsapi.org/v1/sources?language=en&country=us')
 	}
 
-	this.getNews = function (id) {
-		return $http.get('https://newsapi.org/v1/articles?source='+id+'&sortBy=top&apiKey=22942f6468254b089d3bb18c5866a8dc')
+	this.getArticles = function (id) {
+		return $http.get('https://newsapi.org/v1/articles?source='+id+'&apiKey=22942f6468254b089d3bb18c5866a8dc')
 	}
 
 	return {
 		getSources: this.getSources,
-		getNews: this.getNews
+		getArticles: this.getArticles
 	};
 
 });
